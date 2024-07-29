a8a21535793383f69e40e353cf0410104b191a1b
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-07-29 11:19:23 +0530

    Changes

--- app/animation/animation.js
+++ app/animation/animation.js
@@ -8,8 +8,8 @@ const Animation = () => {
       document.querySelector(
         ".section"
       ).style.clipPath = `circle(${value}px at center center)`;
-      document.querySelector(".text").style.left = `${100 - value / 5}%`;
-      document.querySelector(".innerText").style.left = `${100 - value / 5}%`;
+
+      document.querySelector(".innerText").style.left = `${100 - value / 15}%`;
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -23,9 +23,7 @@ const Animation = () => {
       <h3 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-[-50px] text-4xl text-[#9e8489]">
         TULAS Scroll Animation
       </h3>
-      <h2 className="fixed top-1/2 left-full transform -translate-y-1/2 whitespace-nowrap z-10 text-[10vw] text-white text">
-        TULAS Animation
-      </h2>
+
       <section className="fixed top-0 w-full h-screen bg-[#9e8489] flex justify-center items-center clip-circle-50px section">
         <h2 className="fixed top-1/2 left-full transform -translate-y-1/2 whitespace-nowrap z-10 text-[10vw] text-white innerText">
           TULAS Animation
